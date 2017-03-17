ed3478b6f78d0c0bd5c55f34e99373aa0e5e856e
manikandanxenon <manikandan@wearexenon.com>
2017-03-17 13:14:00 +0530

    Added get products and warehouses

--- test/test.js
+++ test/test.js
@@ -7,10 +7,19 @@ var ul = new Unleashed({
   apiId: '7d83c61e-b387-4ea6-babf-45b83cf4a1b1',
   apiKey: 'lcHDEpL3way3buYgemlunU3s66NgebB48kqlBLBBPGTj9qTm2hbXdOk3MBr6gAZmDNrFSiniaoPUE0H2TVbQ=='
 }, function(err, unleashed) {
-  console.log(err, unleashed);
   unleashed.SalesOrders.get({
-    pageSize: 200
+    OrderNumber: 'SO-00000124'
   }, 1, function(err, body) {
+    console.log(err, body);
+  });
+  unleashed.Products.get({
+
+  }, 1, function(err, body) {
+    console.log(err, body);
+  });
+  unleashed.Warehouses.get({
+
+  }, function(err, body) {
     console.log(err, body);
   });
 });
