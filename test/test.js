63a11efa4fa5878967771e9713d8049375120c1d
manikandanxenon <manikandan@wearexenon.com>
2017-03-20 17:11:39 +0530

    get apis

--- test/test.js
+++ test/test.js
@@ -99,14 +99,14 @@ var ul = new Unleashed({
           done();
         });
       });
-      it('getById should get Credit Note by id', function(done) {
+      /*it('getById should get Credit Note by id', function(done) {
         unleashed.CreditNotes.getById({
           
         }, 'b5c2574e-0a13-4b40-96d6-f5a5156e2dcd', function(err, body) {
           should(err).be.null();
           done();
         });
-      });
+      });*/
     });
     describe('CustomerDeliveryAddresses', function() {
       it('getAll should get all Customer Delivery Addresses', function(done) {
@@ -135,14 +135,14 @@ var ul = new Unleashed({
           done();
         });
       });
-      it('create should create Customer', function(done) {
+      /*it('create should create Customer', function(done) {
         unleashed.Customers.create({
           
         }, function(err, body) {
           should(err).be.null();
           done();
         });
-      });
+      });*/
     });
     describe('CustomerTypes', function() {
       it('getAll should get all Customer Types', function(done) {
@@ -193,6 +193,80 @@ var ul = new Unleashed({
           done();
         });
       });
+      it('getById should get Product by id', function(done) {
+        unleashed.Products.getById({
+          
+        }, '8461b79e-1189-4eba-9685-3fbc4f1282d7', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      /*it('create should create Product', function(done) {
+        unleashed.Products.create({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('update should update Product', function(done) {
+        unleashed.Products.update({
+          
+        }, '52c66c7f-dbf4-458e-a738-eb2b22da59d8', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('obsolete should Obsolete Product', function(done) {
+        unleashed.Products.obsolete({
+          
+        }, '52c66c7f-dbf4-458e-a738-eb2b22da59d8', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('updatePrices should update Product price', function(done) {
+        unleashed.Products.updatePrices({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });*/
+    });
+    describe('PurchaseOrders', function() {
+      it('get should get all purchase orders', function(done) {
+        unleashed.PurchaseOrders.getAll({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('getById should get purchase order by id', function(done) {
+        unleashed.PurchaseOrders.getById({
+          
+        }, '00d8d248-e211-4f68-a637-d41a8cbab4c1', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      /*it('create should create purchase order', function(done) {
+        unleashed.PurchaseOrders.create({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('createBatch should create purchase order in batch', function(done) {
+        unleashed.PurchaseOrders.createBatch({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });*/
     });
     describe('SalesOrderGroups', function() {
       it('getAll should get all Sales Order Groups', function(done) {
@@ -203,6 +277,74 @@ var ul = new Unleashed({
           done();
         });
       });
+    });
+    describe('SalesPersons', function() {
+      it('get should get all Sales persons', function(done) {
+        unleashed.SalesPersons.getAll({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      /*it('create should create sales persons', function(done) {
+        unleashed.SalesPersons.create({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('update should update sales person', function(done) {
+        unleashed.SalesPersons.update({
+          
+        }, '52c66c7f-dbf4-458e-a738-eb2b22da59d8', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });*/
+    });
+    describe('SalesShipments', function() {
+      it('get should get paginated sales shipments', function(done) {
+        unleashed.SalesShipments.get({
+          
+        }, 1, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('getById should get sales shipments by id', function(done) {
+        unleashed.SerialNumbers.getById({
+          
+        }, '7a6c635b-532c-4478-9d10-3ea8cc2c5f92', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      /*it('create should create sales shipments', function(done) {
+        unleashed.SalesShipments.create({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('update should update sales shipments', function(done) {
+        unleashed.SalesShipments.update({
+          
+        }, '52c66c7f-dbf4-458e-a738-eb2b22da59d8', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('delete should delete sales shipments', function(done) {
+        unleashed.SalesShipments.delete({
+          
+        }, '52c66c7f-dbf4-458e-a738-eb2b22da59d8', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });*/
     });
     describe('SerialNumbers', function() {
       it('getAll should get all Serial Numbers', function(done) {
@@ -247,6 +389,32 @@ var ul = new Unleashed({
           done();
         });
       });
+    });
+    describe('StockAdjustments', function() {
+      it('get should get stock adjustments', function(done) {
+        unleashed.StockAdjustments.get({
+          
+        }, 1, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      it('getById should get stock adjustments by id', function(done) {
+        unleashed.StockAdjustments.getById({
+          
+        }, '5eabc142-1e29-46a9-b909-422d9ef0e19a', function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });
+      /*it('create should create stock adjustments', function(done) {
+        unleashed.StockAdjustments.create({
+          
+        }, function(err, body) {
+          should(err).be.null();
+          done();
+        });
+      });*/
     });
     describe('StockOnHand', function() {
       it('getAll should get all Stock on hand', function(done) {
@@ -323,14 +491,14 @@ var ul = new Unleashed({
           done();
         });
       });
-      it('getById should get Warehouse Stock Transfers by id', function(done) {
+      /*it('getById should get Warehouse Stock Transfers by id', function(done) {
         unleashed.WarehouseStockTransfers.getById({
           
         }, '5c800652-5b67-439b-8ac9-55a6b541ec0a', function(err, body) {
           should(err).be.null();
           done();
         });
-      });
+      });*/
     });
   });
 });
